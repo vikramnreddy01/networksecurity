71a0b975efdd7c1b99fb1225fc803f067d784071
vikramnreddy01 <vikram.n1999@gmail.com>
2024-12-14 16:43:14 +0530

    done5

--- app.py
+++ app.py
@@ -79,6 +79,7 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
         table_html = df.to_html(classes='table table-striped')
         #print(table_html)
         return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+    ##ggman
         
     except Exception as e:
             raise NetworkSecurityException(e,sys)
