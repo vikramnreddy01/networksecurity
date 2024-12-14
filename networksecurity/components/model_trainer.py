0358dd8901b86f11a25c4392614038c3b9c7db52
vikramnreddy01 <vikram.n1999@gmail.com>
2024-12-14 17:45:25 +0530

    done6

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -27,7 +27,7 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
-dagshub.init(repo_owner='vikramnreddy01', repo_name='networksecurity', mlflow=True)
+#dagshub.init(repo_owner='vikramnreddy01', repo_name='networksecurity', mlflow=True)
 
 os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/vikramnreddy01/networksecurity.mlflow"
 os.environ["MLFLOW_TRACKING_USERNAME"]="vikramnreddy01"
